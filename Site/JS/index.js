36081cdb0997a65338aec93d100f79b5b2e7240a
antonio sousa <antonio.sousa@sptech.school>
2023-11-09 22:58:09 -0300

    Mechendo_no_JS

--- Site/JS/index.js
+++ Site/JS/index.js
@@ -10,117 +10,65 @@ function MostrarConfig(){
     }
 }
 
-var posicao = 2;
-function VoltarTexto(){
+function base1(){
     var conteudoBox = document.getElementById('textinho');
 
     conteudoBox.classList.add("fade-out");
-    posicao -=1;
-    if(posicao == 1){
     setTimeout(function() {
-        box2.style.display = 'none';
-        box1.style.display = 'block';
-        conteudoBox.innerHTML = `
-        <li>
-            <h1>"Dizem que não morremos quando permanecemos na memória de quem amamos"</h1>
-            <h2>-Brian</h2>
-        </li>
-        `;
-        // Remove a classe de animação após trocar o texto para permitir futuras transições
-        conteudoBox.classList.remove("fade-out");
-        
-        posicao = 4;
-    }, 1000);
-    
-    } else if(posicao == 2){
-        setTimeout(function() {
-            box1.style.display = 'block';
-            box2.style.display = 'none';
-            conteudoBox.innerHTML = `
-            <li>
-                <h1>"Velocidade é isso, sentir-se livre"</h1>
-                <h2>-Toretto</h2>
-            </li>
-            `;
-            // Remove a classe de animação após trocar o texto para permitir futuras transições
-            conteudoBox.classList.remove("fade-out");
-            
-        }, 1000);    
-        
-    } else if(posicao == 3){
-        setTimeout(function() {
-            box2.style.display = 'block';
-            box3.style.display = 'none';
-            conteudoBox.innerHTML = `
-            <li>
-                <h1>"Não importa o quanto você corra. Ninguém deixa o passado para trás."</h1>
-                <h2>-Toretto</h2>
-            </li>
-            `;
-            // Remove a classe de animação após trocar o texto para permitir futuras transições
-            conteudoBox.classList.remove("fade-out");
-            
-        }, 1000);
-        
-    }
-
-}
-
-function PassarTexto(){
-    var conteudoBox = document.getElementById('textinho');
-
-    conteudoBox.classList.add("fade-out");
- 
-    posicao +=1
-    if(posicao == 1){
-        setTimeout(function() {
-        box3.style.display = 'none';
-        box1.style.display = 'block';
-
-        conteudoBox.innerHTML = `
-        <li>
-            <h1>"Dizem que não morremos quando permanecemos na memória de quem amamos"</h1>
-            <h2>-Brian</h2>
-        </li>
-        `;
-        // Remove a classe de animação após trocar o texto para permitir futuras transições
-        conteudoBox.classList.remove("fade-out");
-        
-    }, 1000);
-    
-    } else if(posicao == 2){
-        setTimeout(function() {
-            box1.style.display = 'none';
-            box2.style.display = 'block';
-            conteudoBox.innerHTML = `
-            <li>
-                <h1>"Velocidade é isso, sentir-se livre"</h1>
-                <h2>-Toretto</h2>
-            </li>
-            `;
-            // Remove a classe de animação após trocar o texto para permitir futuras transições
-            conteudoBox.classList.remove("fade-out");
-            
-        }, 1000);    
-        
-    } else if(posicao == 3){
-        posicao = 0;
-        setTimeout(function() {
-        box2.style.display = 'none';
+        box1.style.display = 'none';
+        box2.style.display = 'block';
         box3.style.display = 'block';
 
-            conteudoBox.innerHTML = `
-            <li>
-                <h1>"Não importa o quanto você corra. Ninguém deixa o passado para trás."</h1>
-                <h2>-Toretto</h2>
-            </li>
-            `;
-            // Remove a classe de animação após trocar o texto para permitir futuras transições
-            conteudoBox.classList.remove("fade-out");
-            
-        }, 1000);
+        conteudoBox.innerHTML = `
+        <li>
+            <h1>"Dizem que não morremos quando permanecemos na memória de quem amamos"</h1>
+            <h2>-Brian</h2>
+        </li>
+        `;
+        conteudoBox.classList.remove("fade-out");
         
-    }
-
-
+    }, 1000);
 }
+
+function base2(){
+    var conteudoBox = document.getElementById('textinho');
+
+    conteudoBox.classList.add("fade-out");
+    setTimeout(function() {
+        box1.style.display = 'block';
+        box2.style.display = 'none';
+        box3.style.display = 'block';
+        
+
+        conteudoBox.innerHTML = `
+        <li>
+            <h1>"Velocidade é isso, sentir-se livre"</h1>
+            <h2>-Toretto</h2>
+        </li>
+        `;
+        conteudoBox.classList.remove("fade-out");
+        
+    }, 1000);
+}
+
+function base3(){
+    var conteudoBox = document.getElementById('textinho');
+
+    conteudoBox.classList.add("fade-out");
+    setTimeout(function() {
+        box1.style.display = 'block';
+        box2.style.display = 'block';
+        box3.style.display = 'none';
+        
+
+        conteudoBox.innerHTML = `
+        <li>
+            <h1>"Não importa o quanto você corra. Ninguém deixa o passado para trás."</h1>
+            <h2>-Toretto</h2>
+        </li>
+        `;
+        conteudoBox.classList.remove("fade-out");
+        
+    }, 1000);
+}
+
