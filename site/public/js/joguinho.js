37556a150f0521a5db210a08f07b62d53420872a
Antonio Gonçalves Sousa <antonio.sousa@sptech.school>
2023-11-24 19:14:55 -0300

    WDV working 100%

---
+++ site/public/js/joguinho.js
@@ -0,0 +1,9 @@
+function informacoes() {
+    var blocoImg = document.getElementById('imagem')
+
+    if(blocoConfig.style.display === 'none'){
+        blocoConfig.style.display = 'block';
+    }else{
+        blocoConfig.style.display = 'none';
+    }
+}
